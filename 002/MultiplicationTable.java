313c539010fbe74fd21fd63666344850572ef9c9
whtsky <whtsky@gmail.com>
2016-09-09 11:22:46 +0800

    homework 002

---
+++ 002/MultiplicationTable.java
@@ -0,0 +1,11 @@
+class MultiplicationTable {
+	public static void main(String[] args) {
+		int x, y;
+		for(x=1; x<=9; x++) {
+			for(y=1; y<=x; y++) {
+				System.out.print(x + "*" + y + "=" + x*y + ' ');
+			}
+			System.out.print('\n');
+		}
+	}
+}
